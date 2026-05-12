ced063b8c5cf92a686811b0ac7101e4596f06862
Masangu Kazadi Deogracia <gmasmas1@gmail.com>
2026-05-12 20:20:10 +0200

    DEV-A: Finalisation infrastructure Jour 4

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define APP_NAME "IntelliEditor UDBL"
+#define DEFAULT_BUFFER_SIZE 1024
+#define MAX_PATH_LEN 260
+#define UTF8_ENCODING 65001 // Code page pour Windows
+
+#endif // CONFIG_H
